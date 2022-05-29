161732546a7f76b3bcce88c9c0ded0346ec5d2fc
denis <sidletskyi.d@donnu.edu.ua>
2022-05-29 03:08:28 -0700

    li refactored

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,23 +8,27 @@ import truck from "../icons/truck2.png"
 const Main = (props) => (
     <div className="main">
         <div className="main__item info">
-            <div className="text">
-                &nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.beginning')}
+            <div className="info__item">
+                <div className="text">
+                    {props.t('content.beginning')}
+                </div>
             </div>
-            <img className="photo" src={photo} alt=""/>
+            <div className="info__item">
+                <img className="photo" src={photo} alt=""/>
+            </div>
         </div>
         <div className="main__item details">
-            <div className="text">
-                <div>{props.t('content.information')}</div>
-                <div>{props.t('content.region1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.region2')}</div>
-                <div>{props.t('content.nip1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.nip2')}</div>
-                <div>{props.t('content.krs1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.krs2')}</div>
-            </div>
+            <ul className="text">
+                <li>{props.t('content.information')}</li>
+                <li>{props.t('content.region1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.region2')}</li>
+                <li>{props.t('content.nip1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.nip2')}</li>
+                <li>{props.t('content.krs1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.krs2')}</li>
+            </ul>
         </div>
         <div className="main__item info">
             <div className="info__item">
                 <div className="text">
-                    &nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.weHave')}
+                    {props.t('content.weHave')}
                 </div>
             </div>
             <div className="info__item">
@@ -37,7 +41,7 @@ const Main = (props) => (
         <div className="main__item info">
             <div className="info__item">
                 <div className="text">
-                    &nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.weNeed')}
+                    {props.t('content.weNeed')}
                 </div>
             </div>
             <div className="info__item info__item_good">
@@ -48,11 +52,11 @@ const Main = (props) => (
             </div>
         </div>
         <div className="main__item details">
-            <div className="text">
-                <div>{props.t('content.details')}</div>
-                <div>{props.t('content.swift_code_1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.swift_code_2')}</div>
-                <div>{props.t('content.iban_1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.iban_2')}</div>
-            </div>
+            <ul className="text">
+                <li>{props.t('content.details')}</li>
+                <li>{props.t('content.swift_code_1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.swift_code_2')}</li>
+                <li>{props.t('content.iban_1')}&nbsp;&nbsp;&nbsp;&nbsp;{props.t('content.iban_2')}</li>
+            </ul>
         </div>
         <div className="main__item info">
             <div className="text">
